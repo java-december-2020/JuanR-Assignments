00ea540808e38ee5a2867f35c76a881ed5c6ca2e
Juan Renteria <Juan@Juans-MacBook-Air.local>
2020-12-02 20:57:31 -0800

    first commit

---
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.print("My name is Juan Renteria");
+        System.out.print("I am 24 years old");
+        System.out.print("My hometown is Redwood City, CA");
+    }
+}
