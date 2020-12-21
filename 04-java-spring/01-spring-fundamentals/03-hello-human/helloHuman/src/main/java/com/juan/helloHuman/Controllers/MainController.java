01ff79f158db571c6db470fb3742c64986f526e0
Juan Renteria <Juan@Juans-MacBook-Air.local>
2020-12-20 20:27:02 -0800

    helloHuman

---
+++ 04-java-spring/01-spring-fundamentals/03-hello-human/helloHuman/src/main/java/com/juan/helloHuman/Controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.juan.helloHuman.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index() {
+		return "index.jsp"; 
+	}
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello() {
+		return "hello.jsp";
+	}
+
+}
